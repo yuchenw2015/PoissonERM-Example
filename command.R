b7b7e5c455528f4017b6ff019a5e0cddb6269d22
yuchenw2015 <37629284+yuchenw2015@users.noreply.github.com>
2024-01-11 09:47:36 -0800

    Update command.R

--- command.R
+++ command.R
@@ -1,5 +1,7 @@
 library(PoissonERM)
 rm(list = ls(all = TRUE))
+# set folder.dir1 as the path to folder "Example1"
+# folder.dir1 is better to be an absolute path
 folder.dir1 <- "PoissonERM-Example/Example1/"
 ModelPoisson(pathRunType = folder.dir1,
              user.input = "user-input.r")
@@ -14,9 +16,13 @@ ReportPoisson(pathRunType = getwd(),
 
 
 rm(list = ls(all = TRUE))
+# set folder.dir2 as the path to folder "Example2"
+# folder.dir2 is better to be an absolute path
 folder.dir2 <- "PoissonERM-Example/Example2/"
 ModelPoisson(pathRunType = folder.dir2,
              user.input = "user-input.r")
 ReportPoisson(pathRunType = getwd(),
               model.RData = "myEnvironment.RData",
               file.name = "Report_no_pred.Rmd")
+
+rm(list = ls(all = TRUE))
